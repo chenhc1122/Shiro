8fb57a4fc20b0475329f914f82ac97d436dc651b
chenhc1122 <865798415@qq.com>
2021-07-28 18:06:22 +0800

    init

--- src/main/java/com/yqz/springbootshiro/config/ShiroConfig.java
+++ src/main/java/com/yqz/springbootshiro/config/ShiroConfig.java
@@ -11,6 +11,9 @@ import org.springframework.context.annotation.Configuration;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 
+
+
+//config在编辑阶段就会执行
 @Configuration
 public class ShiroConfig {
 
